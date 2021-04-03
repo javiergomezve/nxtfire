e05d02496bb9c20b04bf4855b6bd6d9a337f23a6
Javier Gomez <javiergomezve@gmail.com>
2021-04-03 18:32:46 -0500

    [ADD] Loading Spinner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import React from "react";
 import Link from 'next/link';
+import Loader from '../components/Loader';
 
 export default function Home() {
     return (
         <div>
+            <Loader show />
+
             <Link prefetch={false} href={{
                 pathname: '/[username]',
                 query: { username: 'javiergomezve'}
