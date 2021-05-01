eb8aba9e661c3a0342fd3114ffe01aac106e20e6
Javier Gomez <javiergomezve@gmail.com>
2021-05-01 17:14:19 -0500

    [FIX] Env variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
+    apiKey: process.env.NEXT_PUBLIC_REACT_APP_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_REACT_APP_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_REACT_APP_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_MESSAGING_SENDER_ID
 };
 
 if (!firebase.apps.length) {
